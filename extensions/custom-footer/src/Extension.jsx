2e2825d6089dde5f2d549b4a3b411319da95706b
Greg Craft <greg.craft@shopify.com>
2024-01-26 12:06:19 -0600

    Updating extension readme and minor code formatting

--- extensions/custom-footer/src/Extension.jsx
+++ extensions/custom-footer/src/Extension.jsx
@@ -8,10 +8,10 @@ import {
 } from '@shopify/ui-extensions-react/checkout';
 
 export default function Extension() {
-  const { storefrontUrl } = useShop();
+  const {storefrontUrl} = useShop();
 
   return (
-    <InlineLayout columns={['auto','fill']}>
+    <InlineLayout columns={['auto', 'fill']}>
       <InlineStack spacing="extraTight" blockAlignment="center">
         <Link to={storefrontUrl}>Home</Link>
         <Icon source="chevronRight" size="extraSmall" />
@@ -20,12 +20,14 @@ export default function Extension() {
         <Text appearance="subdued">Checkout</Text>
       </InlineStack>
 
-      <InlineStack spacing="tight" inlineAlignment='end'>
+      <InlineStack spacing="tight" inlineAlignment="end">
         <Link to={new URL('/sizing', storefrontUrl).href}>Sizing</Link>
         <Link to={new URL('/terms', storefrontUrl).href}>Terms</Link>
         <Link to={new URL('/privacy', storefrontUrl).href}>Privacy</Link>
         <Link to={new URL('/faq', storefrontUrl).href}>FAQ</Link>
-        <Link to={new URL('/accessibility', storefrontUrl).href}>Accessibility</Link>
+        <Link to={new URL('/accessibility', storefrontUrl).href}>
+          Accessibility
+        </Link>
       </InlineStack>
     </InlineLayout>
   );
